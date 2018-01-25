c45de0a9ebff18630b1a72cc8bcac7c7f038ebc4
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-01-25 04:14:40 -0800

    add: Added menu

---
+++ Chelsea & Alphonso/SideMenu.swift
@@ -0,0 +1,21 @@
+//
+//  SideMenu.swift
+//  Chelsea & Alphonso
+//
+//  Created by user on 1/25/18.
+//  Copyright © 2018 Alphonso. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Menu: UIViewController {
+    @IBAction func userDidTap(_ sender: Any) {
+        self.dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
